543e7fd088926a8400dc84bfc2a18ed1d2f6c7c3
Mejan Ro <mrostami@ucsd.edu>
2018-02-22 10:54:25 -0800

    was in lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+// exports.view = function(request, response){
+//  	response.render('viewAlt', projects);
+//};
+
 exports.view = function(request, response){
   	response.render('index', projects);
 };
